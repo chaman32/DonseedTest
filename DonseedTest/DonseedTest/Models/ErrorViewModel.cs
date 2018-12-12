d66fea4a7cebe05a55fb4b24aca0673cebbc56d8
Alejandro Scalise <alejandro.scalise@accessbookings.com>
2018-12-12 11:01:36 +0000

    Init

---
+++ DonseedTest/DonseedTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DonseedTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
